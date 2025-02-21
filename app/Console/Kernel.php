2363059ab743b8d6c0f2fa94d215fc8cca31f587
Felipe Mateus <felipefmfelipe@gmail.com>
2025-02-21 20:03:05 -0300

    feat-dev: analytics feature (#25)

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -53,6 +53,7 @@ class Kernel extends ConsoleKernel
         $schedule->command('app:account-post-count-stat-update')->everySixHours(25)->onOneServer();
         $schedule->command('app:instance-update-total-local-posts')->twiceDailyAt(1, 13, 45)->onOneServer();
         $schedule->command('app:send-weekly-inactive-users')->weeklyOn(0, '10:00')->onOneServer();
+        $schedule->command('app:update-pages-view-statitics')->everyTenMinutes();
 
         if (app()->environment('production')) {
             $schedule->command('app:send-weekly-popular-posts')->weeklyOn(1, '08:00')->onOneServer();
