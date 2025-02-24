ecaa10fd8f79f7c97e7cb2232cbf4e402fe39cd7
Felipe Mateus <eu@felipemateus.com>
2025-02-24 19:12:18 -0300

    update front build

--- resources/views/site/fediverse.blade.php
+++ resources/views/site/fediverse.blade.php
@@ -7,16 +7,7 @@
   </div>
   <hr>
   <section>
-<<<<<<< HEAD
-    <p class="lead"><a href="http://fediverse.party/" rel="nofollow noopener">Fediverse</a> {{__('site.is_a_portmanteau_of_federation_and_universe_etc')}}</p>
-    <p class="lead font-weight-bold text-muted mt-4 mb-0">{{__('site.supported_fediverse_projects')}}</p>
-    <p class="small text-muted">{{__('site.some_of_the_better_known_fediverse_projects_include')}}</p>
-    <ul class="lead pl-4">
-      <li><a href="https://joinmastodon.org" rel="nofollow noopener">Mastodon</a> – {{__('site.a_federated_microblogging_alternative')}}</li>
-    </ul>
-=======
     {!! __('fediverse.body') !!}
->>>>>>> dev
   </section>
 @endsection
 
