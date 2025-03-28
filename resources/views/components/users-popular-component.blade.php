51193fc62a4e7ca5bdf3dba205bcec63ddddb54a
Felipe Mateus <eu@felipemateus.com>
2025-03-27 23:30:55 -0300

    Update users-popular-component.blade.php

--- resources/views/components/users-popular-component.blade.php
+++ resources/views/components/users-popular-component.blade.php
@@ -3,7 +3,7 @@
         <div style="max-width: 400px; margin: 20px auto; border: 1px solid #ddd; padding: 20px; text-align: center; font-family: Arial, sans-serif; border-radius: 10px;">
             <img src="{{ $profile->avatarUrl() }}" alt="Avatar - {{$profile->username }} " style="width: 100px; height: 100px; border-radius: 50%; margin-bottom: 10px;">
             <h2 style="margin: 0; font-size: 20px;">{{  '@'.$profile->username }}</h2>
-            <div style="display: flex; justify-content: center; margin-top: 10px;">
+            <div style="display: flex; justify-content: center; margin-top: 10px; text-align: center;">
                 <div style="text-align: center; margin: 0 10px;">
                     <strong style="display: block; font-size: 16px;">{{ $profile->statusCount() }}</strong>
                     <span style="font-size: 12px; color: #666;">Publicações</span>
