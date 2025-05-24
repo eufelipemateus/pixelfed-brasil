dbeadd0d94e8a1c7962665c3c05121bdf8865f10
Felipe Mateus <eu@felipemateus.com>
2025-05-24 00:47:46 -0300

    fix desactivate logic

--- app/Jobs/InternalPipeline/DesactiveInactiveUserJob.php
+++ app/Jobs/InternalPipeline/DesactiveInactiveUserJob.php
@@ -61,7 +61,6 @@ class DesactiveInactiveUserJob implements ShouldQueue
         User::whereNull('status')
             ->whereNull('deleted_at')
             ->whereNotNull('last_active_at')
-            ->whereNotNull("email_verified_at")
             ->where('last_active_at', '<', now()->subDays(180))
             ->chunk(
                 1000,
