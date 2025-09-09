12fd77088424e355596c244bc6cb99b82a67048a
Felipe Mateus <eu@felipemateus.com>
2025-09-09 00:47:48 -0300

    Update profile-schema-generator.blade.php

--- resources/views/components/profile-schema-generator.blade.php
+++ resources/views/components/profile-schema-generator.blade.php
@@ -1,35 +1,41 @@
 @section('schema')
 @if($settings['crawlable'])
 <script type="application/ld+json">
-{
-  "@@context": "https://schema.org",
-  "@@type": "ProfilePage",
-  "dateCreated": "{{ $profile->created_at->format(DateTime::ATOM) }}",
-  "dateModified": "{{ $profile->updated_at->format(DateTime::ATOM) }}",
-  "mainEntity": {
-    "@@type": "Person",
-    "name": "{{ $profile->name }}",
-    "alternateName": "{{ $profile->username }}",
-    "identifier": "{{ $profile->id }}",
-    "interactionStatistic": [
-      {
-        "@@type": "InteractionCounter",
-        "interactionType": "https://schema.org/FollowAction",
-        "userInteractionCount": {{ $settings['followers']['count'] ? $profile->followers_count : 0 }}
-      },
-    ],
-    "agentInteractionStatistic": {
-      "@@type": "InteractionCounter",
-      "interactionType": "https://schema.org/WriteAction",
-      "userInteractionCount": {{ $profile->status_count }}
-    },
-    "description": "{{ $profile->bio }}",
-    "image": "{{ $profile->avatar_url }}",
-    "sameAs": [
-      "{{$profile->website}}"
-    ]
-  }
-}
+    {
+        "@@context": "https://schema.org",
+        "@@type": "ProfilePage",
+        "dateCreated": "{{ $profile->created_at->format(DateTime::ATOM) }}",
+        "dateModified": "{{ $profile->updated_at->format(DateTime::ATOM) }}",
+        "mainEntity": {
+            "@@type": "Person",
+            "name": "{{ $profile->name }}",
+            "alternateName": "{{ $profile->username }}",
+            "identifier": "{{ $profile->id }}",
+            "interactionStatistic": [{
+                "@@type": "InteractionCounter",
+                "interactionType": "https://schema.org/FollowAction",
+                "userInteractionCount": {
+                    {
+                        $profile - > followerCount()
+                    }
+                }
+            }, ],
+            "agentInteractionStatistic": {
+                "@@type": "InteractionCounter",
+                "interactionType": "https://schema.org/WriteAction",
+                "userInteractionCount": {
+                    {
+                        $profile - > statusCount()
+                    }
+                }
+            },
+            "description": "{{ $profile->bio }}",
+            "image": "{{ $profile->avatarUrl() }}",
+            "sameAs": [
+                "{{$profile->website}}"
+            ]
+        }
+    }
 </script>
 @endif
 @endsection
