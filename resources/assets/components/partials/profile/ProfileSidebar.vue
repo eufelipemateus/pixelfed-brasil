6c61d8ccd5db8735f20e1d420bb054d427892148
Felipe Mateus <eu@felipemateus.com>
2026-01-08 18:31:27 -0300

    update assets

--- resources/assets/components/partials/profile/ProfileSidebar.vue
+++ resources/assets/components/partials/profile/ProfileSidebar.vue
@@ -330,11 +330,13 @@
 			</div>
 
 			<div class="d-none d-md-flex sidebar-sitelinks">
-				<router-link to="/i/web/help">{{ $t('navmenu.help') }}</router-link>
-				<router-link to="/i/web/language">{{ $t('navmenu.language') }}</router-link>
-				<a href="/terms">{{ $t('navmenu.privacy') }}</a>
+                <router-link to="/i/web/language">
+                    <i class="fal fa-language fa-2x" alt="Select a language"></i>
+                </router-link>
+				<router-link to="/help">{{ $t('navmenu.help') }}</router-link>
+				<a href="/privacy">{{ $t('navmenu.privacy') }}</a>
 				<a href="/terms">{{ $t('navmenu.terms') }}</a>
-                <a href="/donate" style="color: #ff6600;" >{{ $t('navmenu.donate') }}</a>
+                <a href="/donate" style="background: #ff6600; color: #fff; border-radius: 20px; font-weight: bold; padding: 6px 18px;">{{ $t('navmenu.donate') }}</a>
 			</div>
 
 			<div class="d-none d-md-block sidebar-attribution">
