5c92ce9b30894ec81a88a8a10c6e8f64d4322862
Felipe Mateus <eu@felipemateus.com>
2026-01-08 23:01:55 -0300

    add rules landing

--- resources/assets/components/landing/Index.vue
+++ resources/assets/components/landing/Index.vue
@@ -8,13 +8,15 @@
 
             <div class="container container-compact">
                 <div class="card bg-bluegray-900" style="border-radius: 10px;">
-                    <div class="card-header bg-bluegray-800 nav-menu" style="border-top-left-radius: 10px; border-top-right-radius: 10px;">
+                    <div class="card-header bg-bluegray-800 nav-menu"
+                        style="border-top-left-radius: 10px; border-top-right-radius: 10px;">
                         <ul class="nav justify-content-around">
                             <li class="nav-item">
                                 <router-link to="/" class="nav-link">{{ $t('site.about') }}</router-link>
                             </li>
                             <li v-if="config.show_directory" class="nav-item">
-                                <router-link to="/web/directory" class="nav-link">{{ $t('site.directory') }}</router-link>
+                                <router-link to="/web/directory" class="nav-link">{{ $t('site.directory')
+                                }}</router-link>
                             </li>
                             <li v-if="config.show_explore_feed" class="nav-item">
                                 <router-link to="/web/explore" class="nav-link">{{ $t('site.explore') }}</router-link>
@@ -23,13 +25,36 @@
                     </div>
 
                     <div class="card-img-top p-2">
-                        <img
-                            :src="config.about.banner_image"
-                            class="img-fluid rounded"
-                            style="width: 100%;max-height: 200px;object-fit: cover;"
-                            alt="Server banner image"
-                            height="200"
-                            onerror="this.src='/storage/headers/default.jpg';this.onerror=null;">
+                        <img :src="config.about.banner_image" class="img-fluid rounded"
+                            style="width: 100%;max-height: 200px;object-fit: cover;" alt="Server banner image"
+                            height="200" onerror="this.src='/storage/headers/default.jpg';this.onerror=null;">
+                    </div>
+
+
+                    <div class="server-admin">
+                        <div class="list-group">
+                            <div v-if="config.contact.account" class="list-group-item bg-transparent">
+                                <p class="item-label">Managed By</p>
+                                <a :href="config.contact.account.url" class="admin-card" target="_blank">
+                                    <div class="d-flex">
+                                        <img :src="config.contact.account.avatar" width="45" height="45" class="avatar"
+                                            :alt="`${config.contact.account.username}'s avatar`"
+                                            onerror="this.src='/storage/avatars/default.jpg';this.onerror=null;">
+
+                                        <div class="user-info">
+                                            <p class="display-name">{{ config.contact.account.display_name }}</p>
+                                            <p class="username">&commat;{{ config.contact.account.username }}</p>
+                                        </div>
+                                    </div>
+                                </a>
+                            </div>
+
+                            <div v-if="config.contact.email" class="list-group-item bg-transparent">
+                                <p class="item-label">Contact</p>
+                                <a :href="`mailto:${config.contact.email}?subject=Regarding ${config.domain}`"
+                                    class="admin-email" target="_blank">{{ config.contact.email }}</a>
+                            </div>
+                        </div>
                     </div>
 
                     <div class="card-body">
@@ -60,31 +85,86 @@
                         </div>
 
                         <div class="server-admin">
-                             </div>
+                            <div class="list-group">
+                                <div v-if="config.contact.account" class="list-group-item bg-transparent">
+                                    <p class="item-label">Managed By</p>
+                                    <a :href="config.contact.account.url" class="admin-card" target="_blank">
+                                        <div class="d-flex">
+                                            <img :src="config.contact.account.avatar" width="45" height="45"
+                                                class="avatar" :alt="`${config.contact.account.username}'s avatar`"
+                                                onerror="this.src='/storage/avatars/default.jpg';this.onerror=null;">
 
+                                            <div class="user-info">
+                                                <p class="display-name">{{ config.contact.account.display_name }}</p>
+                                                <p class="username">&commat;{{ config.contact.account.username }}</p>
+                                            </div>
+                                        </div>
+                                    </a>
+                                </div>
+
+                                <div v-if="config.contact.email" class="list-group-item bg-transparent">
+                                    <p class="item-label">Contact</p>
+                                    <a :href="`mailto:${config.contact.email}?subject=Regarding ${config.domain}`"
+                                        class="admin-email" target="_blank">{{ config.contact.email }}</a>
+                                </div>
+                            </div>
+                        </div>
                         <div class="accordion" id="accordion">
-                          <div class="card bg-bluegray-700">
-                            <div class="card-header bg-bluegray-800" id="headingOne">
-                              <h2 class="mb-0">
-                                <button class="btn btn-link btn-block" type="button" data-toggle="collapse" data-target="#collapseOne" aria-controls="collapseOne" @click="toggleAccordion(0)">
-                                    <span class="text-white h5">
-                                        <i class="far fa-info-circle mr-2 text-muted"></i>
-                                        Sobre o Pixelfed Brasil
-                                    </span>
-                                    <i class="far" :class="[ accordionTab === 0 ? 'fa-chevron-left text-primary': 'fa-chevron-down']"></i>
-                                </button>
-                              </h2>
+                            <div class="card bg-bluegray-700">
+                                <div class="card-header bg-bluegray-800" id="headingOne">
+                                    <h2 class="mb-0">
+                                        <button class="btn btn-link btn-block" type="button" data-toggle="collapse"
+                                            data-target="#collapseOne" aria-controls="collapseOne"
+                                            @click="toggleAccordion(0)">
+                                            <span class="text-white h5">
+                                                <i class="far fa-info-circle mr-2 text-muted"></i>
+                                                Sobre o Pixelfed Brasil
+                                            </span>
+                                            <i class="far"
+                                                :class="[accordionTab === 0 ? 'fa-chevron-left text-primary' : 'fa-chevron-down']"></i>
+                                        </button>
+                                    </h2>
+                                </div>
+
+                                <div id="collapseOne" class="collapse show" aria-labelledby="headingOne"
+                                    data-parent="#accordion">
+                                    <div class="card-body about-text">
+                                        <p>Comunidade voltada para usuários brasileiros que buscam privacidade e
+                                            federação.</p>
+                                        <hr class="border-secondary">
+                                        <p v-html="config.about.description"></p>
+                                    </div>
+                                </div>
                             </div>
 
-                            <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordion">
-                              <div class="card-body about-text">
-                                <p>Comunidade voltada para usuários brasileiros que buscam privacidade e federação.</p>
-                                <hr class="border-secondary">
-                                <p v-html="config.about.description"></p>
-                              </div>
+                            <div class="card bg-bluegray-700">
+                                <div class="card-header bg-bluegray-800" id="headingTwo">
+                                    <h2 class="mb-0">
+                                        <button class="btn btn-link btn-block text-left collapsed" type="button"
+                                            data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false"
+                                            aria-controls="collapseTwo" @click="toggleAccordion(1)">
+                                            <span class="text-white h5">
+                                                <i class="far fa-list mr-2 text-muted"></i>
+                                                {{ $t('site.serverRules') }}
+                                            </span>
+                                            <i class="far"
+                                                :class="[accordionTab === 1 ? 'fa-chevron-left text-primary' : 'fa-chevron-down']"></i>
+                                        </button>
+                                    </h2>
+                                </div>
+                                <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo"
+                                    data-parent="#accordion">
+                                    <div class="card-body">
+                                        <div class="list-group list-group-rules">
+                                            <div v-for="rule in config.rules" class="list-group-item bg-bluegray-900">
+                                                <div class="rule-id">{{ rule.id }}</div>
+                                                <div class="rule-text">{{ rule.text }}</div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
-                          </div>
-                          </div>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -93,44 +173,44 @@
     </div>
 </template>
 <script type="text/javascript">
-	export default {
-		data() {
-			return {
-				config: window.pfl,
-				accordionTab: undefined,
+export default {
+    data() {
+        return {
+            config: window.pfl,
+            accordionTab: undefined,
 
-			}
-		},
+        }
+    },
 
-		methods: {
-			toggleAccordion(idx) {
-				if(this.accordionTab == idx) {
-					this.accordionTab = undefined;
-					return;
-				}
-				this.accordionTab = idx;
-			},
+    methods: {
+        toggleAccordion(idx) {
+            if (this.accordionTab == idx) {
+                this.accordionTab = undefined;
+                return;
+            }
+            this.accordionTab = idx;
+        },
 
-			formatCount(val) {
-				if(!val) {
-					return 0;
-				}
-                const userLocale = navigator.language || 'en-CA';
-				return val.toLocaleString(userLocale, { compactDisplay: "short", notation: "compact"});
-			},
+        formatCount(val) {
+            if (!val) {
+                return 0;
+            }
+            const userLocale = navigator.language || 'en-CA';
+            return val.toLocaleString(userLocale, { compactDisplay: "short", notation: "compact" });
+        },
 
-			formatBytes(bytes, unit = 'megabyte') {
-				const units = ['byte', 'kilobyte', 'megabyte', 'gigabyte', 'terabyte'];
-				const navigatorLocal = navigator.languages && navigator.languages.length >= 0 ? navigator.languages[0] : 'en-US';
-				const unitIndex = Math.max(0, Math.min(Math.floor(Math.log(bytes) / Math.log(1024)), units.length - 1));
-				return Intl.NumberFormat(navigatorLocal, {
-				    style: 'unit',
-					unit : units[unitIndex],
-					useGrouping: false,
-					maximumFractionDigits: 0,
-					roundingMode: 'ceil'
-				}).format(bytes / (1024 ** unitIndex))
-			}
-		}
-	}
+        formatBytes(bytes, unit = 'megabyte') {
+            const units = ['byte', 'kilobyte', 'megabyte', 'gigabyte', 'terabyte'];
+            const navigatorLocal = navigator.languages && navigator.languages.length >= 0 ? navigator.languages[0] : 'en-US';
+            const unitIndex = Math.max(0, Math.min(Math.floor(Math.log(bytes) / Math.log(1024)), units.length - 1));
+            return Intl.NumberFormat(navigatorLocal, {
+                style: 'unit',
+                unit: units[unitIndex],
+                useGrouping: false,
+                maximumFractionDigits: 0,
+                roundingMode: 'ceil'
+            }).format(bytes / (1024 ** unitIndex))
+        }
+    }
+}
 </script>
