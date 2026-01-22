dfa834311b71aed2cb7bb368f4d2ebbfc9c53fa3
Felipe Mateus <eu@felipemateus.com>
2026-01-21 23:42:30 -0300

    adjust about

--- resources/views/site/about.blade.php
+++ resources/views/site/about.blade.php
@@ -5,26 +5,46 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="mobile-web-app-capable" content="yes">
+    <title>{{ __('site.about_title') }}</title>
+    <meta name="description" content="{{ __('site.about_journey') }}">
+    <meta property="og:type" content="website">
+    <meta property="og:url" content="https://pixelfed.com.br/about">
+    <meta property="og:title" content="Pixelfed Brasil - A Maior Instância Brasileira do Fediverso">
+    <meta property="og:description" content="Rede social de fotos ética, federada e sem algoritmos. Descubra a soberania digital na maior instância Pixelfed do Brasil.">
+    <meta property="og:image" content="https://pixelfed.com.br/img/pixelfed.png">
 
-    <title>Nossa História | Pixelfed Brasil</title>
-    <meta name="description" content="Conheça a jornada do Pixelfed Brasil: Iniciada em 2022 e reconstruída em 2023. Soberania digital, LGPD e fotografia ética.">
+    <meta property="twitter:card" content="summary_large_image">
+    <meta property="twitter:url" content="https://pixelfed.com.br/about">
+    <meta property="twitter:title" content="Pixelfed Brasil">
+    <meta property="twitter:description" content="A maior instância brasileira do Pixelfed. Fotografia ética e federada.">
+    <meta property="twitter:image" content="https://pixelfed.com.br/img/pixelfed.png">
 
     <script type="application/ld+json">
         {
-            "@context": "https://schema.org",
-            "@type": "Organization",
+            "@@context": "https://schema.org",
+            "@@type": "Organization",
+            "@@id": "https://pixelfed.com.br/#organization",
             "name": "Pixelfed Brasil",
-            "alternateName": "Pixelfed.com.br",
+            "alternateName": ["Pixelfed.com.br", "Pixelfed BR"],
             "url": "https://pixelfed.com.br",
             "logo": "https://pixelfed.com.br/img/pixelfed.png",
             "description": "A maior instância brasileira do Pixelfed. Uma rede social de compartilhamento de fotos federada, ética e sem algoritmos de manipulação, parte do Fediverso.",
-            "foundingDate": "2023",
+            "foundingDate": "2022",
+            "founder": {
+                "@@type": "Person",
+                "name": "Felipe Mateus",
+                "url": "https://pixelfed.com.br/eufelipemateus",
+                "sameAs": [
+                    "https://github.com/eufelipemateus",
+                    "https://felipemateus.com"
+                ]
+            },
             "address": {
-                "@type": "PostalAddress",
+                "@@type": "PostalAddress",
                 "addressCountry": "BR"
             },
             "contactPoint": {
-                "@type": "ContactPoint",
+                "@@type": "ContactPoint",
                 "email": "suporte@felipemateus.com",
                 "contactType": "customer support",
                 "url": "https://pixelfed.com.br/contact"
@@ -51,22 +71,76 @@
         <div class="container">
             <p class="text-right mt-3">
                 <a href="/" class="font-weight-bold text-dark">{{ __('site.home') }}</a>
-                <a href="{{route('newsroom.index')}}" class="ml-4 font-weight-bold text-dark">{{ __('site.newsroom') }}</a>
             </p>
         </div>
 
         <div class="px-4 py-5 my-5 text-center">
             <a href="/">
-                <img class="d-block mx-auto mb-4" src="/img/pixelfed-icon-color.svg" alt="Logo" width="72" height="57">
+                <img class="d-block mx-auto mb-4" src="/img/pixelfed_transparent.png" alt="Logo Pixelfed Brasil" width="120" style="filter: drop-shadow(0 2px 8px #10c5f855);">
             </a>
-            <div class="local-badge">{{ __('site.manifest_history_badge') }}</div>
+            <div class="local-badge">Pixelfed Brasil</div>
             <h1 class="display-4 font-weight-bold py-3">{{ __('site.about_title') }}</h1>
-            <div class="col-lg-6 mx-auto">
-                <p class="lead mb-4 font-weight-light" style="font-size: 24px; line-height: 1.5;">
-                    {{ __('site.about_journey') }}
+            <div class="col-lg-7 mx-auto">
+                <p class="lead mb-4 font-weight-light" style="font-size: 22px; line-height: 1.5;">
+                    {!! __('site.about_intro') !!}
                 </p>
             </div>
         </div>
+
+        <div class="container mb-4">
+            <div class="row align-items-stretch">
+                <div class="col-md-12 mb-4">
+                    <div class="card h-100 shadow-lg history-card p-4">
+                        <h2 class="font-weight-bold mb-3"><i class="fas fa-bullseye icon-bullet"></i> {{ __('site.our_mission') }}</h2>
+                        <p class="mb-0" style="font-size:1.08rem;">
+                            {!! __('site.our_mission_text') !!}
+                        </p>
+                    </div>
+                </div>
+                <div class="col-md-12 mb-4">
+                    <div class="card h-100 shadow-lg history-card p-4 border-primary" style="border-width: 2px !important;">
+                        <h2 class="font-weight-bold mb-3"><i class="fas fa-star icon-bullet"></i> {{ __('site.why_pixelfed_brasil') }}</h2>
+                        <ul class="mb-0" style="font-size:1.08rem;">
+                            <li><b>{{ __('site.benefit_ethics_title') }}:</b> {{ __('site.benefit_ethics_text') }}</li>
+                            <li><b>{{ __('site.benefit_ads_title') }}:</b> {{ __('site.benefit_ads_text') }}</li>
+                            <li><b>{{ __('site.benefit_data_title') }}:</b> {{ __('site.benefit_data_text') }}</li>
+                            <li><b>{{ __('site.benefit_local_title') }}:</b> {{ __('site.benefit_local_text') }}</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="row align-items-stretch">
+                <div class="col-md-6 mb-4">
+                    <div class="card h-100 shadow-lg history-card p-4">
+                        <h2 class="font-weight-bold mb-3"><i class="fas fa-user-astronaut icon-bullet"></i> {{ __('site.who_makes_pixelfed_brasil') }}</h2>
+                        <p class="mb-0" style="font-size:1.08rem;">
+                            {{ __('site.founder_intro') }}
+
+                            <ul class="list-unstyled mt-3">
+                            <li>{!! __('site.founder_name') !!} (<a href="https://pixelfed.com.br/eufelipemateus" target="_blank">Perfil</a>)</li>
+                            <li><b>Links:</b> <a href="https://github.com/eufelipemateus" target="_blank">GitHub</a> | <a href="https://felipemateus.com" target="_blank">Site pessoal</a></li>
+                        </ul>
+                        </p>
+                    </div>
+                </div>
+                <div class="col-md-6 mb-4">
+                    <div class="card h-100 shadow-lg history-card p-4 border-success" style="border-width: 2px !important;">
+                        <h2 class="font-weight-bold mb-3"><i class="fas fa-heart icon-bullet"></i> {{ __('site.support_our_instance') }}</h2>
+                        <p class="mb-2" style="font-size:1.08rem;">
+                            {{ __('site.support_our_instance_text') }}
+                        </p>
+                        <p class="mb-0" style="font-size:1.08rem;">
+                            {{ __('site.apoie_cta') }}
+                            <a href="{{ route('site.donate') }}" class="btn  btn-sm ml-2" style="background: #ff6600; color: #fff;" target="_blank"><i class="fas fa-hand-holding-heart mr-2"></i> {{ __('site.donate') }}</a>
+                        </p>
+                        <ul class="list-unstyled mt-3">
+                            <li><b>{{ __('site.apoie_links') }}:</b> <a href="https://apoia.se/pixelfedbrasil" target="_blank">{{ __('site.apoie_apoia') }}</a> | <a href="https://pixelfed.com.br/PixelfedBrasil" target="_blank">{{ __('site.apoie_perfil') }}</a> | <a href="https://github.com/eufelipemateus/pixelfed-brasil" target="_blank">{{ __('site.apoie_github') }}</a></li>
+                        </ul>
+                    </div>
+                </div>
+
+        </div>
+        <div class="section-spacer"></div>
 
         <div class="container">
             <div class="row align-items-stretch">
@@ -102,6 +176,8 @@
             </div>
         </div>
 
+
+
         <div class="section-spacer"></div>
 
         <div class="container">
@@ -124,6 +200,26 @@
                     <hr>
                     <h2 class="display-4 font-weight-bold text-primary">100%</h2>
                     <p class="h4 font-weight-light">{{ __('site.chronological_order') }}</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="section-spacer"></div>
+
+
+        <div class="container ">
+            <div class="row align-items-stretch">
+                <div class="col-md-12 mb-4">
+                    <div class="card h-100 shadow-lg history-card p-4 border-info" style="border-width: 2px !important;">
+                        <h2 class="font-weight-bold mb-3"><i class="fas fa-envelope-open-text icon-bullet"></i> {{ __('site.contact_admins') }}</h2>
+                        <p class="mb-2" style="font-size:1.08rem;">{{ __('site.you_can_contact_the_admins') }}</p>
+                        <ul class="mb-2" style="font-size:1.08rem;">
+                            <li><b>{{ __('site.contact-us') }}:</b> <a href="https://pixelfed.com.br/contact" target="_blank">pixelfed.com.br/contact</a></li>
+                            <li><b>{{ __('site.contact_by_email') }}</b> <a href="mailto:suporte@felipemateus.com">suporte@felipemateus.com</a></li>
+                            <li><b>{{ __('site.localization') }}:</b> Brasil</li>
+                        </ul>
+                        <p class="mb-0" style="font-size:1.08rem;"><b>{{ __('site.contact_type')  }}</b> Suporte ao usuário</p>
+                    </div>
                 </div>
             </div>
         </div>
